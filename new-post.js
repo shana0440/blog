688f6018ec154f5132b6536898f9bad0c05f9916
kwguo <shana0440@gmail.com>
2020-02-24 02:41:00 +0800

    add script to add new post

---
+++ new-post.js
@@ -0,0 +1,23 @@
+const fs = require("fs")
+const path = require("path")
+
+const title = process.argv[2]
+const dirName = title.toLocaleLowerCase().replace(/\s/g, "-")
+
+const dirPath = path.resolve(__dirname, "content", "blog", dirName)
+
+fs.mkdirSync(dirPath)
+
+const template = `
+---
+title: ${title}
+date: "${new Date().toISOString()}"
+description: "here is your description"
+---
+
+Start typing something
+`
+
+const postPath = path.resolve(dirPath, "index.md")
+fs.writeFileSync(postPath, template.trim())
+console.log(postPath)
