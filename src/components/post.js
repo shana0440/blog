b82a991232d5be245400874b2e6044c704bf556e
kwguo <shana0440@gmail.com>
2022-09-17 00:02:18 +0800

    feat: add active style to post item

--- src/components/post.js
+++ src/components/post.js
@@ -5,7 +5,11 @@ import { Link } from "gatsby"
 const Post = ({ node }) => {
   const title = node.frontmatter.title || node.fields.slug
   return (
-    <Link to={`/writings${node.fields.slug}`}>
+    <Link
+      to={`/writings${node.fields.slug}`}
+      className="rounded-md py-1 px-2 mb-1 hover:bg-gray-100 block"
+      activeClassName="bg-gray-100"
+    >
       <article key={node.fields.slug}>
         <header>
           <h3>{title}</h3>
--- src/pages/writings.js
+++ src/pages/writings.js
@@ -39,11 +39,11 @@ const WritingsIndex = ({ children }) => {
       <SEO title="My writings" />
       <div className="flex">
         <ul
-          className="flex flex-col gap-4 h-screen overflow-auto w-96 p-2 border-r border-r-gray-300"
+          className="flex flex-col gap-2 h-screen overflow-auto w-96 p-2 border-r border-r-gray-300"
           {...scrollRestoration}
         >
           {posts.map(({ node }, i) => (
-            <li key={i} className="rounded-md py-1 px-2 mb-1 hover:bg-gray-100">
+            <li key={i}>
               <Post node={node} />
             </li>
           ))}
