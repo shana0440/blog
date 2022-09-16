a5f7352d632acadd480c3bcbdc3930c61d8330d1
kwguo <shana0440@gmail.com>
2022-09-16 23:52:33 +0800

    feat: update 404 page ui

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,29 +1,27 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title
-
+const NotFoundPage = () => {
   return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Layout>
+      <SEO title="Not Found" />
+      <div className="py-24 px-5 text-center">
+        <h1 className="font-bold text-lg">
+          The page you are looking for is not exists
+        </h1>
+        <p className="text-gray-500 mb-5 text-lg">find your way home</p>
+        <Link
+          to="/"
+          className="px-2 py-1 bg-gray-100 rounded-md border border-gray-300 text-gray-500 hover:bg-gray-200"
+        >
+          Go home
+        </Link>
+      </div>
     </Layout>
   )
 }
 
 export default NotFoundPage
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-  }
-`
